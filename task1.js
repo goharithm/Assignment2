606902bdf0fd0e6e48f4ee3d8cbe3f860056ac55
Goharithm <goharithm@gmail.com>
2025-12-08 15:53:22 +0200

    test 1

--- task1.js
+++ task1.js
@@ -1,1 +1,108 @@
-console.log("hello world !! ")
+// 1. Write a function that logs the current file path and directory. (0.5 Grade)
+// • Output Example:{File:“/home/user/project/index.js”, Dir:“/home/user/project”}
+
+console.log("teast this * ")
+
+// 2. Write a function that takes a file path and returns its file name. (0.5 Grade)
+// • Input Example: /user/files/report.pdf
+// • Output Example:"report.pdf"
+
+
+
+// 3. Write a function that builds a path from an object (0.5 Grade)
+// • Input Example: { dir: "/folder", name: "app", ext: ".js"}
+// • Output Example: “/folder/app.js”
+
+
+// 4. Write a function that returns the file extension from a given file path. (0.5 Grade)
+// • Input Example: /docs/readme.md"
+// • Output Example: “.md”
+
+
+// 5. Write a function that parses a given path and returns its name and ext. (0.5 Grade)
+// • Input Example: /home/app/main.js
+// • Output Example: { Name: “main”, Ext: “.js” }
+
+
+// 6. Write a function that checks whether a given path is absolute. (0.5 Grade)
+// • Input Example: /home/user/file.txt
+// • Output Example: true
+
+
+// 7. Write a function that joins multiple segments (0.5 Grade)
+// • Input:"src","components", "App.js"
+// • Output Example: src/components/App.js
+
+
+// 8. Write a function that resolves a relative path to an absolute one. (0.5 Grade)
+// • Input Example: ./index.js
+// • Output Example: /home/user/project/src/index.js
+
+
+// 9. Write a function that joins two paths. (0.5 Grade)
+// • Input Example: /folder1, folder2/file.txt
+// • Output Example: /folder1/folder2/file.txt
+
+
+// 10. Write a function that deletes a file asynchronously. (0.5 Grade)
+// • Input Example: /path/to/file.txt
+// • Output Example: The file.txt is deleted.
+
+
+// 11. Write a function that creates a folder synchronously. (1 Grade)
+// • Output Example: “Success”
+
+
+// 12. Create an event emitter that listens for a "start" event and logs a welcome message. (0.5 Grade)
+// • Output Example: Welcome event triggered!
+
+
+
+// 13. Emit a custom "login" event with a username parameter. (0.5 Grade)
+// • Input Example:"Ahmed"
+// • Output Example: “User logged in: Ahmed”
+
+
+
+// 14. Read a file synchronously and log its contents. (1 Grade)
+// • Input Example:"./notes.txt"
+// • Output Example: the file content => “This is a note.”
+
+
+
+// 15. Write asynchronously to a file. (1 Grade)
+// • Input: path:"./async.txt", content:"Async save"
+
+
+
+// 16. Check if a directory exists. (0.5 Grade)
+// • Input Example: "./notes.txt"
+// • Output Example: true
+
+
+
+// 17. Write a function that returns the OS platform and CPU architecture. (0.5 Grade)
+// • Output Example: {Platform: “win32”, Arch: “x64”}
+//
+
+
+
+// Assignment2
+//
+// 2
+//
+
+// C. Bonus (3 Grades):
+// How to deliver the bonus?
+
+
+// 1- Solve the problem kth-missing-positive-number on LeetCode
+
+
+
+// 2- Inside your assignment folder, create a SEPARATE FILE and name it “bonus.js”
+
+
+
+
+// 3- Copy the code that you have submitted on the website inside ”bonus.js” file
